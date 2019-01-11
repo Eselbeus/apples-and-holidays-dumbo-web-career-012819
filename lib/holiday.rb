318be21796b93da013f3ca8fa4ba845eafe3e5be
eselbeus <eselbeus@gmail.com>
2019-01-11 22:53:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
       #   end
       # end
       
-      puts format_holiday + ": " + items.join(", ")
+      puts "  " + format_holiday + ": " + items.join(", ")
       
     end
   end
